4f0b22083d57f6c2470ec82a2992cca46f6acaac
glbrom <golbrm@gmail.com>
2023-01-04 07:33:29 +0200

    Module 7-9

--- Twin Game/Twin Game/TwinThemeChooserViewController.swift
+++ Twin Game/Twin Game/TwinThemeChooserViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TwinThemeChooserViewController: UIViewController {
+class TwinThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
 
     let themes = [
         "Animals":"🐼🐷🙈🦆🐛🐞🪰🐼🐶🦐",
@@ -15,12 +15,49 @@ class TwinThemeChooserViewController: UIViewController {
         "Foods":"🍉🍌🥝🥥🥦🍐🍓🥑🥓🥩"
     ]
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        splitViewController?.delegate = self
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        if let cvc = secondaryViewController as? TwinViewController {
+            if cvc.theme == nil {
+                return true
+            }
+        }
+        return false
+    }
+        
+    @IBAction func changeTheme(_ sender: Any) {
+        if let cvc = splitViewDetailTwinViewCController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+        } else if let cvc = lastSeguedTwinViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailTwinViewCController: TwinViewController? {
+        return splitViewController?.viewControllers.last as? TwinViewController
+    }
+    
+    // MARK: - Navigation
+    
+    private var lastSeguedTwinViewController: TwinViewController?
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Choose Theme" {
             if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
                 if let cvc = segue.destination as? TwinViewController {
                     cvc.theme = theme
+                    lastSeguedTwinViewController = cvc
                 }
             }
         }
